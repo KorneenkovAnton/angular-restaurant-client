c94d09b0118e2f6b9efe7f222b31706530487876
KorneenkovAnton <41143644+KorneenkovAnton@users.noreply.github.com>
2020-05-25 01:04:05 +0600

    restaurant admin app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/angular-restaurant-client'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/angular-restaurant-client/src/app/app.component.html'));
+  res.sendFile(path.join(__dirname+'/dist/angular-restaurant-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
